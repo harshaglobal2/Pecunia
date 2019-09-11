2aba61528cbbbaaccb8c93ac6e6435a01e31f026
harshaglobal2 <harshaglobal2@gmail.com>
2019-09-11 14:18:08 +0530

    11.9.2019

---
+++ 07) 11.9.2019/RegexExample/RegexExample/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexExample
+{
+    class Customer
+    {
+        private string _customerName;
+        private string _email;
+
+        public string CustomerName
+        {
+            set
+            {
+                Regex regex = new Regex("^[a-zA-Z ]*$");
+                bool b = regex.IsMatch(value);
+                if (b == true)
+                {
+                    _customerName = value;
+                }
+                else
+                {
+                    throw new Exception("CustomerName should contain alphabets only");
+                }
+            }
+            get
+            {
+                return _customerName;
+            }
+        }
+
+        public string Email
+        {
+            set
+            {
+                Regex regex = new Regex(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*");
+                bool b = regex.IsMatch(value);
+                if (b == true)
+                {
+                    _email = value;
+                }
+                else
+                {
+                    throw new Exception("Email is invalid");
+                }
+            }
+            get
+            {
+                return _email;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Customer customer = new Customer();
+                Console.WriteLine("Customer Name:");
+                customer.CustomerName = Console.ReadLine();
+                Console.WriteLine(customer.CustomerName);
+
+                Console.WriteLine("Email:");
+                customer.Email = Console.ReadLine();
+                Console.WriteLine(customer.Email);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 07) 11.9.2019/ThreadingExample/ThreadingExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingExample
+{
+    class Sample
+    {
+        public void Method1()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.Write($"i={i} ");
+            }
+        }
+
+        public void Method2()
+        {
+            for (int j = 1; j <= 1000; j++)
+            {
+                Console.Write($"j={j} ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Sample sample = new Sample();
+
+            Thread thread1 = new Thread(sample.Method1);
+            Thread thread2 = new Thread(sample.Method2);
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine("Done");
+
+            Console.ReadKey();
+        }
+    }
+}
