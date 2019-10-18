100570633ada8e385efc976c8b59987e329f1f97
harshaglobal2 <harshaglobal2@gmail.com>
2019-10-18 15:20:32 +0530

    18.10.2019

---
+++ 14) 18.10.2019/EFExample/EFExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            companyEntities companyEntities = new companyEntities();
+
+            List<Employee> employees = 
+                companyEntities.Employees
+                .Where(emp => emp.EmpName == "Scott")
+                .ToList();
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine($"{item.EmpID},{item.EmpName},{item.Salary}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 14) 18.10.2019/EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using Company.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            companyEntities entites = new companyEntities();
+            List<Employee> employees = entites.Employees.ToList();
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine($"{item.EmpID},{item.EmpName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
