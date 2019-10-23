f733246b48fb7e4773fc987059f478164cd46fb1
harshaglobal2 <harshaglobal2@gmail.com>
2019-10-23 14:05:29 +0530

    23.10.2019

---
+++ 17) 23.10.2019/jQueryPractice/jQueryPractice/JavaScript.js
@@ -0,0 +1,23 @@
+﻿//selecting & css
+//$("#div1").css("background-color", "lightgreen");
+
+//set html
+//$("#div1").html("<p>new para</p>");
+
+//get html
+//var s = $("#div1").html();
+//console.log(s);
+
+//before
+$(document).ready(function ()
+{
+    $("#div1").before("<p>new para</p>");
+});
+
+
+$(window).load(function ()
+{
+    $("#div1").before("<p>new para</p>");
+});
+
+
---
+++ 17) 23.10.2019/jQueryPractice/jQueryPractice/JavaScript1.js
@@ -0,0 +1,38 @@
+﻿var employees = [
+    { empID: 101, empName: "Scott", salary: 5000 },
+    { empID: 102, empName: "Smith", salary: 6540 },
+    { empID: 103, empName: "Allen", salary: 9400 },
+    { empID: 104, empName: "Jones", salary: 2240 }
+];
+
+$(document).ready(function ()
+{
+    for (let emp of employees)
+    {
+        var htmlString = `<tr> 
+        <td>${emp.empID}</td> 
+        <td>${emp.empName}</td> 
+        <td>${emp.salary}</td>
+        <td><button type="button" class='editbtn'>Edit</button></td>
+    </tr>`;
+        $("#employeesTbody").append(htmlString);
+    }
+});
+
+$(document).on("click", '.editbtn', function (event)
+{
+    var currentTr = $(event.target).parent().parent();
+    var currentEmpID = currentTr.find("td:first").html();
+
+    var emp = employees.filter(function (e)
+    {
+        return e.empID == currentEmpID;
+    });
+    //console.log(emp[0].empID);
+    //console.log(emp[0].empName);
+    //console.log(emp[0].salary);
+
+    $("#txtEmpID").val(emp[0].empID);
+    $("#txtEmpName").val(emp[0].empName);
+    $("#txtSalary").val(emp[0].salary);
+});
