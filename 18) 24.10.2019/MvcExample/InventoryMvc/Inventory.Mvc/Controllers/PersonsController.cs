1cbcf88a4a6626a9823d77b10833e67f6d473a4d
harshaglobal2 <harshaglobal2@gmail.com>
2019-10-24 13:12:23 +0530

    24.10.2019

---
+++ 18) 24.10.2019/MvcExample/InventoryMvc/Inventory.Mvc/Controllers/PersonsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Mvc.Models;
+
+namespace Inventory.Mvc.Controllers
+{
+    public class PersonsController : Controller
+    {
+        // URL: Persons/Create
+        public ActionResult Create()
+        {
+            //Creating and initializing viewmodel object
+            PersonViewModel personViewModel = new PersonViewModel()
+            {
+                PersonName = "Harsha",
+                DateOfJoining = DateTime.Now
+            };
+
+            //Calling view & passing viewmodel object to view
+            return View(personViewModel);
+        }
+    }
+}
---
+++ 18) 24.10.2019/MvcExample/InventoryMvc/Inventory.Mvc/Models/PersonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventory.Mvc.Models
+{
+    public class PersonViewModel
+    {
+        public System.Guid PersonID { get; set; }
+        public string PersonName { get; set; }
+        public string Email { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<System.DateTime> DateOfJoining { get; set; }
+        public string Gender { get; set; }
+        public bool IsRegistered { get; set; }
+        public string State { get; set; }
+    }
+}
+
